a4cd317dd8502739c78e2827ab8bf724c1459df6
aahspringaa4 <aahspringaa4@naver.com>
2021-10-05 09:43:46 +0900

    [Feat] 클라이언트 구현

--- app/src/main/java/com/example/chat_app/MyPageActivity.kt
+++ app/src/main/java/com/example/chat_app/MyPageActivity.kt
@@ -7,5 +7,6 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
     }
 }
