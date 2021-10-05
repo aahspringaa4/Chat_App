afc5a68ead5991d0e856ce136179a041afd52571
aahspringaa4 <aahspringaa4@naver.com>
2021-10-05 09:43:22 +0900

    [Fix] 오류 수정 (startActivity)

--- app/src/main/java/com/example/chat_app/JoinActivity.kt
+++ app/src/main/java/com/example/chat_app/JoinActivity.kt
@@ -17,6 +17,7 @@ class JoinActivity : AppCompatActivity() {
 
         binding.ibRegister.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
