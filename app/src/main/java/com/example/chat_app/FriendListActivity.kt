0f9fe59c9a7ce7182ba9a301d27d5ff3c657a36b
aahspringaa4 <aahspringaa4@naver.com>
2021-10-05 09:42:46 +0900

    [Feat] 기초 기능 구현

--- app/src/main/java/com/example/chat_app/FriendListActivity.kt
+++ app/src/main/java/com/example/chat_app/FriendListActivity.kt
@@ -7,5 +7,6 @@ class FriendListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_list)
+
     }
 }
