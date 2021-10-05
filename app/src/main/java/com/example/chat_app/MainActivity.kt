26c87c74ccbefa514f114bd32f2ba9aa41878732
aahspringaa4 <aahspringaa4@naver.com>
2021-10-05 09:43:35 +0900

    [Feat] 기초 기능 추가

--- app/src/main/java/com/example/chat_app/MainActivity.kt
+++ app/src/main/java/com/example/chat_app/MainActivity.kt
@@ -12,7 +12,6 @@ import com.example.chat_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
