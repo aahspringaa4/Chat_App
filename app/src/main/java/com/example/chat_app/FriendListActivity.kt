a53ebac471cfba98702726023d1c7425a12368dc
aahspringaa4 <aahspringaa4@naver.com>
2021-10-07 22:29:47 +0900

    [Feat] 클라이언트 구축

--- app/src/main/java/com/example/chat_app/FriendListActivity.kt
+++ app/src/main/java/com/example/chat_app/FriendListActivity.kt
@@ -2,11 +2,20 @@ package com.example.chat_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat_app.databinding.ActivityFriendListBinding
+import com.example.chat_app.databinding.ActivityJoinBinding
 
 class FriendListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFriendListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_friend_list)
+        binding = ActivityFriendListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.chat.setOnClickListener {
+            finish()
+        }
     }
 }
