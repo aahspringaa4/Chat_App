69eb190ed90cd39aa8b17e9e46361fe512e77054
aahspringaa4 <aahspringaa4@naver.com>
2021-10-13 23:14:21 +0900

    [Feat] 파일 이름 수정

--- app/src/main/java/com/example/chat_app/ui/fragment/Home.kt
+++ app/src/main/java/com/example/chat_app/ui/fragment/Home.kt
@@ -1,17 +1,44 @@
 package com.example.chat_app.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chat_app.R
+import com.example.chat_app.ui.recyclerview.ChattingAdapter
+import com.example.chat_app.ui.recyclerview.ChattingData
 
 class home : Fragment() {
+    lateinit var rv: RecyclerView
+    lateinit var ChattingAdapter: ChattingAdapter
+    val datas = mutableListOf<ChattingData>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        val view = inflater.inflate(R.layout.home, container, false) //frag1과 연결시켜 return해줌.
-
+        val view = inflater.inflate(R.layout.activity_chat, container, false) //frag1과 연결시켜 return해줌.
+        rv = view.findViewById(R.id.rv)
+        initRecycler()
         return view
+    }
+
+    private fun initRecycler() {
+        ChattingAdapter = ChattingAdapter(context)
+        rv.adapter = ChattingAdapter
+
+
+        datas.apply {
+            add(ChattingData(img = R.drawable.dream, id = "명철이 형", content = "1일 1커밋", people = "2명"))
+            Log.d("결과", "성공")
+//            add(FriendListData(img = R.drawable.profile3, name = "jenny", age = 26))
+//            add(FriendListData(img = R.drawable.profile2, name = "jhon", age = 27))
+//            add(FriendListData(img = R.drawable.profile5, name = "ruby", age = 21))
+//            add(FriendListData(img = R.drawable.profile4, name = "yuna", age = 23))
+
+            ChattingAdapter.datas = datas
+            ChattingAdapter.notifyDataSetChanged()
+
+        }
     }
 }
