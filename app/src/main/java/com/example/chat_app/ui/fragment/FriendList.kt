a7c7833b136dd31510c159ac9d4280387c2e658c
aahspringaa4 <aahspringaa4@naver.com>
2021-10-13 22:51:17 +0900

    [Fix] RecycleView 오류 수정

--- app/src/main/java/com/example/chat_app/ui/fragment/FriendList.kt
+++ app/src/main/java/com/example/chat_app/ui/fragment/FriendList.kt
@@ -1,6 +1,7 @@
 package com.example.chat_app.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,22 +14,24 @@ import com.example.chat_app.ui.RecyclerView.FriendListData
 class FriendList : Fragment() {
 
     lateinit var rv: RecyclerView
-        lateinit var profileAdapter: FriendListAdapter
-        val datas = mutableListOf<FriendListData>()
+    lateinit var friendlistAdapter: FriendListAdapter
+    val datas = mutableListOf<FriendListData>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.activity_friend_list, container, false) //frag1과 연결시켜 return해줌.
-
         rv = view.findViewById(R.id.rv)
+        initRecycler()
         return view
     }
 
     private fun initRecycler() {
-        var friendlistAdapter = FriendListAdapter(this)
+        friendlistAdapter = FriendListAdapter(context)
         rv.adapter = friendlistAdapter
 
 
         datas.apply {
             add(FriendListData(img = R.drawable.dream, id = "명철이 형", content = "1일 1커밋"))
+            Log.d("결과", "성공")
 //            add(FriendListData(img = R.drawable.profile3, name = "jenny", age = 26))
 //            add(FriendListData(img = R.drawable.profile2, name = "jhon", age = 27))
 //            add(FriendListData(img = R.drawable.profile5, name = "ruby", age = 21))
