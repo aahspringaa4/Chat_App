fa9da87aaf6339b7e31da39953549f92707e4ac0
aahspringaa4 <aahspringaa4@naver.com>
2021-10-15 11:58:59 +0900

    [Feat] Mypage Fragment Class -> Activity 로 변경

---
+++ app/src/main/java/com/example/chat_app/ui/activity/MypageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chat_app.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+    }
+}
