aefb5929a4e4e59202cc09b0cd7b8248c9e736e8
aahspringaa4 <aahspringaa4@naver.com>
2021-10-15 11:52:59 +0900

    [Fix] Fragment 오류 수정

--- app/src/main/java/com/example/chat_app/ui/fragment/Mypage.kt
+++ app/src/main/java/com/example/chat_app/ui/fragment/Mypage.kt
@@ -9,11 +9,7 @@ import com.example.chat_app.R
 
 class Mypage : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-
-
         val view = inflater.inflate(R.layout.activity_my_page, container, false) //frag1과 연결시켜 return해줌.
-
         return view
     }
 }
