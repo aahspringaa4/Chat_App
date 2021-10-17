30c2dbb38bc6af7cd87263e19358ff481978370b
aahspringaa4 <aahspringaa4@naver.com>
2021-10-17 23:30:59 +0900

    [Feat] 기능 추가

--- app/src/main/java/com/example/chat_app/ui/activity/MypageActivity.kt
+++ app/src/main/java/com/example/chat_app/ui/activity/MypageActivity.kt
@@ -1,12 +1,28 @@
 package com.example.chat_app.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chat_app.R
+import com.example.chat_app.databinding.ActivityHomeBinding
+import com.example.chat_app.databinding.ActivityMypageBinding
 
 class MypageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMypageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage)
+        binding = ActivityMypageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.talk.setOnClickListener {
+            var intent = Intent(this, ChattingActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvChat.setOnClickListener {
+            var intent = Intent(this, ChattingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
