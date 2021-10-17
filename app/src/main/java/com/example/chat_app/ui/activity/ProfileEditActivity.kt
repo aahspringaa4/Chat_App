cc2dbf1633e3b6114ad6c7408dd11d7ad8fb2552
aahspringaa4 <aahspringaa4@naver.com>
2021-10-17 23:29:51 +0900

    [Feat] Layout 및 XML 추가

---
+++ app/src/main/java/com/example/chat_app/ui/activity/ProfileEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chat_app.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_edit)
+    }
+}
