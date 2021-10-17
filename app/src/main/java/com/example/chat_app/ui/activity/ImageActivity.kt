b821f8ac2ae70122240d70f98f1f07d5bb0e99e8
aahspringaa4 <aahspringaa4@naver.com>
2021-10-17 23:30:46 +0900

    [Feat] ImageActivity 기능 추가

--- app/src/main/java/com/example/chat_app/ui/activity/ImageActivity.kt
+++ app/src/main/java/com/example/chat_app/ui/activity/ImageActivity.kt
@@ -1,12 +1,42 @@
 package com.example.chat_app.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chat_app.R
+import com.example.chat_app.databinding.ActivityHomeBinding
+import com.example.chat_app.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image)
+        binding = ActivityImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backbutton.setOnClickListener {
+            finish()
+        }
+
+        binding.talk.setOnClickListener {
+            val intent = Intent(this, ChattingActivity::class.java)
+            startActivity(intent)
+        }
+        binding.followchat.setOnClickListener {
+            val intent = Intent(this, ChattingActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.profileedit.setOnClickListener {
+            val intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvProfile.setOnClickListener {
+            val intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
