5d11ea22a54ffff59dfa80590dea9a27475e3881
aahspringaa4 <aahspringaa4@naver.com>
2021-11-02 23:03:22 +0900

    [Feat] Spinner & Register 구현

--- app/src/main/java/ui/activity/login/JoinActivity.kt
+++ app/src/main/java/ui/activity/login/JoinActivity.kt
@@ -1,8 +1,13 @@
 package ui.activity.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chat_app.databinding.ActivityJoinBinding
+
 
 class JoinActivity : AppCompatActivity() {
 
@@ -13,8 +18,42 @@ class JoinActivity : AppCompatActivity() {
         binding = ActivityJoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.ibRegister.setOnClickListener {
+        binding.tvSignIn.setOnClickListener{
             finish()
+        }
+
+        binding.ibRegister.setOnClickListener {
+            startActivity(Intent (this, AccountActivity::class.java))
+        }
+
+        binding.ChooseSex.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                binding.etRegisterSex.setText(parent?.getItemAtPosition(position).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {]
+            }
+
+        }
+    }
+
+    private fun Register() {
+
+        val Birth: String = binding.etRegisterBirth.getText().toString()
+        val phone: String = binding.etRegisterPhone.getText().toString()
+        val sex: String = binding.etRegisterSex.getText().toString()
+
+        hideKeyboard()
+        
+        if (Birth.trim { it <= ' ' }.length == 0 || phone.trim { it <= ' ' }.length == 0 || sex.trim { it <= ' ' }.length == 0 || Birth == null || phone == null || sex == null) {
+            Toast.makeText(this@JoinActivity, "올바른 정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
+        } else {
+            RegisterResponse()
         }
     }
 }
