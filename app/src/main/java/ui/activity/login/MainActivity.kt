a799a74a1c1079f1624934a1f32a0040666004e1
aahspringaa4 <aahspringaa4@naver.com>
2021-11-02 23:03:42 +0900

    [Fix] 오류 수정

--- app/src/main/java/ui/activity/login/MainActivity.kt
+++ app/src/main/java/ui/activity/login/MainActivity.kt
@@ -1,6 +1,5 @@
 package ui.activity.login
 
-import network.ApiService
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.chat_app.databinding.ActivityMainBinding
 import model.RequestLoginDTO
 import model.ResponseLoginDTO
+import network.ApiService
 import network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,8 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var id: String? = null
     var password: String? = null
-    var token: String? = null
-    var user_id: Int? = null
+    var data: String? = null
+    var count: Int? = null
     private var ApiService: ApiService? = null
     private var retrofitClient: RetrofitClient? = null
 
@@ -45,8 +45,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 
     private fun Login() {
@@ -97,7 +95,7 @@ class MainActivity : AppCompatActivity() {
 
                         startActivity(Intent(this@MainActivity, HomeActivity::class.java))
                     }
-                    if (response.code() == 401) {
+                    else if (response.code() == 401) {
                         Toast.makeText(
                             this@MainActivity,
                             "로그인에 실패하였습니다.\n(아이디 또는 비밀번호를 다시 확인해주세요)",
@@ -108,7 +106,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<ResponseLoginDTO?>?, t: Throwable?) {
-                Log.d("error","${t}")
             }
         })
     }
