7d5f75bd14d8efe4fb591d278e51334e106debb9
aahspringaa4 <aahspringaa4@naver.com>
2021-11-02 20:10:55 +0900

    [Feat] Base URL

---
+++ app/src/main/java/network/RetrofitClient.java
@@ -0,0 +1,35 @@
+package network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient instance = null;
+    private static ApiService ApiService;
+    //사용하고 있는 서버 BASE 주소
+    private static String baseUrl = "http://54.180.98.98:9094/";
+
+
+
+    private RetrofitClient() {
+
+        //retrofit 설정
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService = retrofit.create(ApiService.class);
+    }
+
+    public static RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
+    public static ApiService getRetrofitInterface() {
+        return ApiService;
+    }
+}
