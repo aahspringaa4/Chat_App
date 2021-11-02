fb2b13a20547804e89ee1910383ed93b0a9bc95b
aahspringaa4 <aahspringaa4@naver.com>
2021-11-02 20:12:12 +0900

    [Feat] DTO

--- app/src/main/java/model/RequestLoginDTO.kt
+++ app/src/main/java/model/RequestLoginDTO.kt
@@ -1,31 +1,8 @@
 package model
 
-public class RequestLoginDTO(username: String, password: String) {
-    var username: String? = null
-    var password: String? = null
+import com.google.gson.annotations.SerializedName
 
-    fun RequestLogin(username: String?, password: String?) {
-        this.username = username
-        this.password = password
-    }
-
-    @JvmName("getUsername1")
-    fun getUsername(): String? {
-        return username
-    }
-
-    @JvmName("setUsername1")
-    fun setUsername(username: String?) {
-        this.username = username
-    }
-
-    @JvmName("getPassword1")
-    fun getPassword(): String? {
-        return password
-    }
-
-    @JvmName("setPassword1")
-    fun setPassword(password: String?) {
-        this.password = password
-    }
-}
+data class RequestLoginDTO(
+    val id: String,
+    val password: String
+)
--- app/src/main/java/model/ResponseLoginDTO.kt
+++ app/src/main/java/model/ResponseLoginDTO.kt
@@ -1,31 +1,9 @@
 package model
 
+import com.google.gson.JsonObject
 import com.google.gson.annotations.SerializedName
 
-public class ResponseLoginDTO {
-    @SerializedName("Bearer-Token")
-    var Token: String? = null
-
-    @SerializedName("User-Id")
-    var user_id = 0
-
-    @JvmName("getUser_id1")
-    fun getUser_id(): Int {
-        return user_id
-    }
-
-    @JvmName("setUser_id1")
-    fun setUser_id(user_id: Int) {
-        this.user_id = user_id
-    }
-
-    @JvmName("getToken1")
-    fun getToken(): String? {
-        return Token
-    }
-
-    @JvmName("setToken1")
-    fun setToken(token: String?) {
-        Token = token
-    }
-}
+data class ResponseLoginDTO (
+    val count: String,
+    val data: Data
+    )
