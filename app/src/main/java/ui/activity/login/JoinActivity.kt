b33469eafdb5c33b40dc8c9e8fa19d4b694d2ed0
aahspringaa4 <aahspringaa4@naver.com>
2021-11-03 22:00:34 +0900

    [Feat] Register

--- app/src/main/java/ui/activity/login/JoinActivity.kt
+++ app/src/main/java/ui/activity/login/JoinActivity.kt
@@ -12,13 +12,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.chat_app.databinding.ActivityAccountBinding
 import com.example.chat_app.databinding.ActivityJoinBinding
 import model.RequestRegisterDTO
+import model.ResponseLoginDTO
 import network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class JoinActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityJoinBinding
     private lateinit var binding2: ActivityAccountBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityJoinBinding.inflate(layoutInflater)
@@ -59,76 +64,8 @@ class JoinActivity : AppCompatActivity() {
         }
     }
 
-    public fun Register() {
 
-        val Birth: String = binding.etRegisterBirth.getText().toString()
-        val phone: String = binding.etRegisterPhone.getText().toString()
-        val sex: String = binding.etRegisterSex.getText().toString()
 
-        hideKeyboard()
 
-        if (Birth.trim { it <= ' ' }.length == 0 || phone.trim { it <= ' ' }.length == 0 || sex.trim { it <= ' ' }.length == 0 || Birth == null || phone == null || sex == null) {
-            Toast.makeText(this@JoinActivity, "올바른 정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
-        } else {
-            RegisterResponse()
-        }
-    }
-
-    fun RegisterResponse() {
-        val birth: String = binding.etRegisterBirth.getText().toString().trim()
-        val phone: String = binding.etRegisterPhone.getText().toString().trim()
-        val gender: String = binding.etRegisterSex.getText().toString().trim()
-
-        val id: String = binding2.etRegisterId.getText().toString().trim()
-        val name: String = binding2.etRegisterName.getText().toString().trim()
-        val password: String = binding2.etRegisterPw.getText().toString().trim()
-
-        // 정보 저장
-        val requestRegister = RequestRegisterDTO(birth, phone, gender, id, name, password)
-        val retrofitClient = RetrofitClient.getInstance()
-        val ApiService = RetrofitClient.getRetrofitInterface()
-
-//        ApiService.Register(requestRegister).enqueue(object : Callback<ResponseLoginDTO?>() {
-//            override fun onResponse(
-//                call: Call<ResponseLoginDTO?>?,
-//                response: Response<ResponseLoginDTO?>
-//            ) {
-//                println("")
-//                if (response.isSuccessful() && response.body() != null) {
-//
-//                    if (response.code() === 200) {
-//                        Toast.makeText(
-//                            this@JoinActivity,
-//                            "회원가입이 성공적으로 완료되었습니다.",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                        finish()
-//                    } else {
-//                        Toast.makeText(
-//                            this@JoinActivity,
-//                            "예기치 못한 오류가 발생했습니다.\n 고객센터에 문의해주세요.",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ResponseLoginDTO?>?, t: Throwable?) {
-//                Toast.makeText(
-//                    this@JoinActivity,
-//                    "예기치 못한 오류가 발생했습니다.\n고객센터에 문의해주세요.",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//        })
-    }
-
-    private fun hideKeyboard() // 키보드 숨기기
-    {
-        val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(binding.etRegisterBirth.getWindowToken(), 0)
-        imm.hideSoftInputFromWindow(binding.etRegisterPhone.getWindowToken(), 0)
-        imm.hideSoftInputFromWindow(binding.etRegisterSex.getWindowToken(), 0)
-    }
 
 }
