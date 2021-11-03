7aa0c70c6e52f9cf28e7efabea86a592587fdf47
aahspringaa4 <aahspringaa4@naver.com>
2021-11-03 10:26:46 +0900

    [Feat] binding, EditText NonNull

--- app/src/main/java/ui/activity/login/AccountActivity.kt
+++ app/src/main/java/ui/activity/login/AccountActivity.kt
@@ -3,6 +3,7 @@ package ui.activity.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chat_app.R
 import com.example.chat_app.databinding.ActivityAccountBinding
 import com.example.chat_app.databinding.ActivityJoinBinding
@@ -17,7 +18,15 @@ class AccountActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.ibRegister.setOnClickListener {
-            startActivity(Intent(this, CheckActivity::class.java))
+            val id: String = binding.etRegisterId.getText().toString()
+            val name: String = binding.etRegisterName.getText().toString()
+            val password: String = binding.etRegisterPw.getText().toString()
+
+            if (id.trim { it <= ' ' }.length == 0 || name.trim { it <= ' ' }.length == 0 || password.trim { it <= ' ' }.length == 0 || id == null || name == null || password == null) {
+                Toast.makeText(this@AccountActivity, "회원가입 정보를 옳바르게 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(Intent(this, CheckActivity::class.java))
+            }
         }
 
         binding.tvSignIn.setOnClickListener {
