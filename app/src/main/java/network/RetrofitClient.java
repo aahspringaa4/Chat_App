62cef7a064804c6a16e260cb9b80f0faa3da3655
aahspringaa4 <aahspringaa4@naver.com>
2021-11-04 19:45:52 +0900

    [Feat] Base URL

--- app/src/main/java/network/RetrofitClient.java
+++ app/src/main/java/network/RetrofitClient.java
@@ -7,9 +7,7 @@ public class RetrofitClient {
     private static RetrofitClient instance = null;
     private static ApiService ApiService;
     //사용하고 있는 서버 BASE 주소
-    private static String baseUrl = "http://54.180.98.98:9094/";
-
-
+    private static String baseUrl = "http://13.209.19.255:9094/";
 
     private RetrofitClient() {
 
