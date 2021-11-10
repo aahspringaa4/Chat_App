1903db22b930c9f1db773daa3097633ad8fb6203
aahspringaa4 <aahspringaa4@naver.com>
2021-11-10 23:16:34 +0900

    [Feat] Chatting Adapter 구현

--- app/src/main/java/adapter/ChattingAdapter.kt
+++ app/src/main/java/adapter/ChattingAdapter.kt
@@ -1,97 +1,94 @@
 package adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.example.chat_app.R
-import model.data.ChatData
+import com.example.chat_app.databinding.ChatmeBinding
+import com.example.chat_app.databinding.ChatotherBinding
+import model.data.ChattingData
+import viewmodel.ChattingViewModel
 
-class ChatAdapter {
-    var data = arrayListOf<ChatData>()
+@Suppress("UNREACHABLE_CODE")
+class ChattingAdapter(val chatting  : MutableLiveData<List<ChattingData>>, val viewModel: ChattingViewModel, val index : Int, val name : String) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val MY_CHAT=0
+    private val YOUR_CHAT=1
 
-    private val MY_CHAT = 0
-    private val YOUR_CHAT = 1
 
-    override fun getItemViewType(position: Int): Int {
-        val chatMessage = data[position]
-
-        return if (chatMessage.id=="me") {
-            MY_CHAT
-        } else {
-            YOUR_CHAT
+    inner class MyChatViewHolder(private val binding : ChatmeBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(position: Int, viewModel: ChattingViewModel) {
+            binding.vm = viewModel
+            binding.position = position
+            binding.executePendingBindings()
         }
     }
 
-    class ChatMyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        val message: TextView = view.findViewById(R.id.tv_chatmyitem_contents)
-
-        fun onBind(chatData: ChatData) {
-            message.text = chatData.message
+    inner class YourChatViewHolder(private val binding : ChatotherBinding): RecyclerView.ViewHolder(binding.root){ // 이것만 이미지 포함
+        fun bind(position: Int, viewModel: ChattingViewModel) {
+            binding.vm = viewModel
+            binding.position = position
+            binding.executePendingBindings()
         }
     }
 
-    class ChatYourViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        val profileImage: ImageView = view.findViewById(R.id.iv_yourchatitem_profile)
-        val message: TextView = view.findViewById(R.id.tv_yourchatitem_message)
-
-        fun onBind(chatData: ChatData) {
-            message.text = chatData.message
-
-            Glide.with(itemView.context).load(chatData.profile)
-                .transform(RoundedCorners(180))
-                .transition(DrawableTransitionOptions.withCrossFade()) to profileImage
-
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
-        lateinit var viewHolder: RecyclerView.ViewHolder
-
-        when (viewType) {
-            MY_CHAT -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.chatme, parent, false)
-
-                return ChatMyViewHolder(view)
-
+        when(viewType){
+            MY_CHAT ->{
+                val binding = ChatmeBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+                return MyChatViewHolder(binding)
             }
-            YOUR_CHAT -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.chatother, parent, false)
-
-                return ChatYourViewHolder(view)
+            YOUR_CHAT ->{
+                val binding = ChatotherBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+                return YourChatViewHolder(binding)
             }
         }
-        return viewHolder
-    }
 
-    override fun getItemCount(): Int {
-        return data.size
+        run { // else 안뜨게 처리 함
+            val binding = ChatmeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return MyChatViewHolder(binding)
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-        when (holder){
-            is ChatMyViewHolder -> {
-                holder.onBind(data[position])
+        when(holder.itemViewType){
+            MY_CHAT ->{
+                (holder as MyChatViewHolder).bind(position,viewModel)
             }
-            is ChatYourViewHolder -> {
-                holder.onBind(data[position])
+            YOUR_CHAT ->{
+                (holder as YourChatViewHolder).bind(position,viewModel)
+            }
+            else -> { // else 안뜨게 처리 함
+
             }
         }
     }
 
-    override fun addItem(item: ChatData) {
-        data.add(item)
+
+    override fun getItemCount(): Int {
+        return when(chatting.value){
+            null -> 0
+            else -> chatting.value!!.size
+        }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return when(index){
+            0 ->{
+                return when(chatting.value?.get(position)?.user){
+                    "1" -> MY_CHAT
+                    "2" -> YOUR_CHAT
+                    else -> 1
+                }
+            }
+            else -> {
+                return when(chatting.value?.get(position)?.user){
+                    "1" -> YOUR_CHAT
+                    "2" -> MY_CHAT
+
+                    else -> 1
+                }
+            }
+        }
+    }
 }
