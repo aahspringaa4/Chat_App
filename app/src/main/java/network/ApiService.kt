52bae9e0fb28520a738864d5872505d96f03a8f4
aahspringaa4 <aahspringaa4@naver.com>
2021-11-10 23:18:01 +0900

    [Feat] Chatting NetWork

--- app/src/main/java/network/ApiService.kt
+++ app/src/main/java/network/ApiService.kt
@@ -1,12 +1,11 @@
 package network
 
-import model.DTO.RequestLoginDTO
-import model.DTO.RequestRegisterDTO
-import model.DTO.ResponseLoginDTO
-import model.DTO.ResponseRegisterDTO
+import model.dto.*
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+
 
 interface ApiService {
 
@@ -16,8 +15,13 @@ interface ApiService {
         @Body requestLoginDTO: RequestLoginDTO
     ): Call<ResponseLoginDTO>
 
+    // 회원가입
     @POST("api/auth/signup")
     fun Register(
         @Body requestRegister: RequestRegisterDTO
     ): Call<ResponseRegisterDTO>
+
+    // 채팅 리스트 조회
+    @GET("api/mypage/chattingroom")
+    fun ChattingList(): Call<ChattingListResponseDTO>
 }
