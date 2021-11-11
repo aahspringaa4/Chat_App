7b3b2af6d7dcadae7653299766064b30dee41af7
aahspringaa4 <aahspringaa4@naver.com>
2021-11-11 20:11:12 +0900

    Feat :: Base Api

--- app/src/main/java/network/BaseApi.java
+++ app/src/main/java/network/BaseApi.java
@@ -4,26 +4,19 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BaseApi {
-    private static BaseApi instance = null;
+    private static Retrofit instance = null;
     private static ApiService ApiService;
     //사용하고 있는 서버 BASE 주소
     private static String baseUrl = "http://92f6-211-36-140-182.ngrok.io/";
 
-    private BaseApi() {
-
-        //retrofit 설정
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ApiService = retrofit.create(ApiService.class);
-    }
-
-    public static BaseApi getInstance() {
-        if (instance == null) {
-            instance = new BaseApi();
+    public static Retrofit getInstance() {
+        if(instance == null) {
+            instance = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
+
         return instance;
     }
 
