0fa24507add90c95b2d4af0b54c1918c4f194031
aahspringaa4 <aahspringaa4@naver.com>
2021-11-15 23:31:08 +0900

    Feat :: Activity

---
+++ app/src/main/java/ui/activity/FriendAddActivity.kt
@@ -0,0 +1,11 @@
+package ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_add)
+    }
+}
