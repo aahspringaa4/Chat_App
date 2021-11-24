81612e90eb45bbcfb8a3cb6dca31bfa6c5acfc68
aahspringaa4 <aahspringaa4@naver.com>
2021-11-24 19:46:38 +0900

    Feat :: Instance 객체 등록

--- app/src/main/java/network/RetrofitClient.java
+++ app/src/main/java/network/RetrofitClient.java
@@ -1,8 +1,27 @@
 package network;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiModule {
-    
+public class RetrofitClient {
+    private static Retrofit instance = null;
+    private static ApiService ApiService;
+    //사용하고 있는 서버 BASE 주소
+    private static String baseUrl = "http://13.209.19.255/9095/";
+
+    public static Retrofit getInstance() {
+        if(instance == null) {
+            instance = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return instance;
+    }
+
+    public static ApiService getRetrofitInterface() {
+        return ApiService;
+    }
 }
