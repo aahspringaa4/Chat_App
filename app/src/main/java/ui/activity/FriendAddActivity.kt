ef81435b189ab60602ab884ad94619380ddcc476
aahspringaa4 <aahspringaa4@naver.com>
2021-11-24 19:46:15 +0900

    Feat :: 친구 추가 (Post)

--- app/src/main/java/ui/activity/FriendAddActivity.kt
+++ app/src/main/java/ui/activity/FriendAddActivity.kt
@@ -2,11 +2,53 @@ package ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.chat_app.R
+import android.widget.Toast
+import com.example.chat_app.databinding.ActivityFriendAddBinding
+import model.dto.RequestFriendListDTO
+import network.ApiService
+import network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import ui.activity.login.HomeActivity
 
 class FriendAddActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFriendAddBinding
+
+    private var ApiService: ApiService? = null
+    private var retrofitClient: RetrofitClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_friend_add)
+        binding = ActivityFriendAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.check.setOnClickListener{
+            finish()
+            pushPost()
+        }
+    }
+    private fun pushPost() {
+
+        var memberId = binding.edFriendId.text.toString()
+
+        retrofitClient = RetrofitClient()
+
+        ApiService = RetrofitClient.getRetrofitInterface()
+
+        val call: Call<RequestFriendListDTO>? = ApiService?.FriendApply(memberId)
+        call?.enqueue(object : Callback<RequestFriendListDTO?> {
+            override fun onResponse(
+                call: Call<RequestFriendListDTO?>,
+                response: Response<RequestFriendListDTO?>
+            ) {
+                Toast.makeText(HomeActivity(), "myText", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onFailure(call: Call<RequestFriendListDTO?>, t: Throwable) {
+                Toast.makeText(HomeActivity(), "myText", Toast.LENGTH_SHORT).show();
+            }
+        })
     }
 }
