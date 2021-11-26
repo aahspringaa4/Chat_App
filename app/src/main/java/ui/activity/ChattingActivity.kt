1976bd167b6534992727c0a8fc7c2af1e437d51d
aahspringaa4 <aahspringaa4@naver.com>
2021-11-26 22:57:58 +0900

    Feat :: Socket Chatting Room

--- app/src/main/java/ui/activity/ChattingActivity.kt
+++ app/src/main/java/ui/activity/ChattingActivity.kt
@@ -2,11 +2,54 @@ package ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
 import com.example.chat_app.R
+import com.example.chat_app.databinding.ActivityChattingBinding
+import kotlinx.coroutines.Dispatchers.IO
+import org.json.JSONObject
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.URISyntaxException
 
 class ChattingActivity : AppCompatActivity() {
+
+    private lateinit var socket: Socket
+    var chattingRoomId: String = "adfbeefc-3307-4ccd-8dbf-3aa2401e4781"
+    val chatBody = MutableLiveData<String>()
+    private lateinit var binding: ActivityChattingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chatting)
+        binding = ActivityChattingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        joinRoom()
+
+        binding.btSend.setOnClickListener {
+            sendChatting()
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun joinRoom() { // 방 입장 소켓
+        val data = JSONObject()
+        data.put("chattingRoomId", chattingRoomId)
+        socket.emit("join_room", data)
+    }
+
+
+    fun sendChatting() { // 보내기 버튼 누르면 실행 소켓
+        if(!chatBody.value.isNullOrEmpty()){
+            val message = chatBody.value
+            val data = JSONObject()
+            data.put("chattingRoomId", chattingRoomId)
+            data.put("message", message)
+            socket.emit("send_chat", data)
+            chatBody.value = ""
+        }
+    }
+
 }
