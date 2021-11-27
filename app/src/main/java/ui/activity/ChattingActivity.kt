e7a465506fd400aba3f88b3c0325c8723d167321
aahspringaa4 <aahspringaa4@naver.com>
2021-11-27 12:45:21 +0900

    Feat :: getchatting  with Rxjava

--- app/src/main/java/ui/activity/ChattingActivity.kt
+++ app/src/main/java/ui/activity/ChattingActivity.kt
@@ -1,14 +1,19 @@
 package ui.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import com.example.chat_app.databinding.ActivityChattingBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers
 import model.dto.RequestEnterChattingRoomDTO
 import model.dto.RequestSendMessageDTO
 import network.SocketApplication
 import io.socket.client.Socket
 import model.dto.RequestLeaveChatDTO
+import network.RetrofitClient
 
 class ChattingActivity : AppCompatActivity() {
 
@@ -17,6 +22,8 @@ class ChattingActivity : AppCompatActivity() {
     private val chatBody = MutableLiveData<String>()
 
     private lateinit var binding: ActivityChattingBinding
+
+    val adapter = RetrofitClient.getRetrofitInterface()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,11 +51,22 @@ class ChattingActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("CheckResult")
+    private fun getChatting() { // 채팅 데이터 가져오기
+        val chattingRoomId: String = "adfbeefc-3307-4ccd-8dbf-3aa2401e4781"
+        val count:Int = 1
+        adapter.getChatting(chattingRoomId , count)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                
+            )
+    }
+
     private fun joinRoom() { // 방 입장 소켓
         val data = RequestEnterChattingRoomDTO()
         socket.emit("joinFriendRoom", data)
     }
-
 
     private fun sendChatting() { // 보내기 버튼 누르면 실행 소켓
         if(!chatBody.value.isNullOrEmpty()){
