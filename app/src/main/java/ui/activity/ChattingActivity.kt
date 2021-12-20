1913ea57ab7f919b24a2931045ecd394b4a001af
유현명 <aahspringaa4@naver.com>
2021-12-21 00:21:56 +0900

    Feat :: 소켓 연동 부분 추가

--- app/src/main/java/ui/activity/ChattingActivity.kt
+++ app/src/main/java/ui/activity/ChattingActivity.kt
@@ -8,13 +8,19 @@ import androidx.lifecycle.MutableLiveData
 import com.example.chat_app.databinding.ActivityChattingBinding
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.socket.client.IO
+import io.socket.client.Manager
 import model.dto.RequestEnterChattingRoomDTO
 import model.dto.RequestSendMessageDTO
 import network.SocketApplication
 import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import io.socket.engineio.client.Transport
 import model.data.ChattingData
 import model.dto.RequestLeaveChatDTO
 import network.RetrofitClient
+import org.json.JSONObject
+import java.net.URISyntaxException
 
 class ChattingActivity() : AppCompatActivity() {
 
@@ -87,4 +93,70 @@ class ChattingActivity() : AppCompatActivity() {
         }
     }
 
+    fun startSocket(accessToken: String) {
+        try {
+            val opts = IO.Options()
+            opts.transports =
+                arrayOf(io.socket.engineio.client.transports.WebSocket.NAME) // xhr에러 방지
+            socket = IO.socket("https://api.eungyeol.live/chat", opts)
+            socket.io().on(Manager.EVENT_TRANSPORT, Emitter.Listener { args ->
+                val trans = args[0] as Transport
+                trans.on(Transport.EVENT_REQUEST_HEADERS) { // request 해더 넣는 부분
+                        args ->
+                    val mHeaders = args[0] as MutableMap<String, List<String>>
+                    println("여기가 실행${accessToken}")
+                    mHeaders["Authorization"] = listOf("Bearer $accessToken")
+                }
+            })
+            socket.on(Socket.EVENT_CONNECT) {
+                println("성공")
+            }.on(Socket.EVENT_CONNECT_ERROR) {
+                println("실패;;")
+                println(it.contentToString()) // 이게 에러 받는거입니다
+            }
+            socket.on("response", connect)
+            socket.connect()
+            socket.on("recv_chat", chat).apply {
+            }
+            socket.on("error", connect)
+        } catch (e: URISyntaxException) {
+            println(e.reason)
+        }
+    }
+
+    val connect: Emitter.Listener = Emitter.Listener {
+        println("헬퍼 성공?")
+        val size = it.size - 1
+        val data = it
+        for (i in 0..size) {
+            println("${data[i]} 이게 연결 결과값")
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    val chat: Emitter.Listener = Emitter.Listener {
+
+        val json = JSONObject(it[0].toString())
+        var result = true
+        val title = json.getString("title")
+        val msg = json.getString("msg")
+        val user_type = json.getString("user_type")
+        val date = json.getString("date")
+        if(json.isNull("result")){
+        }else{
+            result = json.getBoolean("result")
+            try {
+                userResult2.value = result
+            }catch (e: Throwable){}
+        }
+
+        try {
+            chatInfo = ChattingData(title,msg,result,user_type,date)
+            possingChat.add(chatInfo)
+            chattingList.postValue(possingChat)
+            chattingListAdapter.notifyDataSetChanged()
+            navigater.binding.chatPageRv.scrollToPosition(possingChat.size)
+        }catch (e:Throwable){}
+        navigater.binding.chatPageRv.smoothScrollToPosition(possingChat.size)
+    }
 }
