b3c18c6520c4b3b67e2307a33114255819e29fab
유현명 <aahspringaa4@naver.com>
2022-01-10 15:57:03 +0900

    📑:: Room Entity

---
+++ domain/src/main/java/com/example/domain/RequestEnterChattingRoomEntity.kt
@@ -0,0 +1,6 @@
+package com.example.domain
+
+data class RequestEnterChattingRoomEntity(
+    val chattingRoomId: String = "adfbeefc-3307-4ccd-8dbf-3aa2401e4781",
+    val chatCategory: String = "GOODS"
+)
