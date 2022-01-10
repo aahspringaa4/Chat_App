8adafeedb90710659d615f33a4116404bfcb56c5
유현명 <aahspringaa4@naver.com>
2022-01-10 16:16:25 +0900

    📑 :: 친구 리스트 Repository 구현

--- domain/src/main/java/com/example/domain/repository/FriendListRepository.kt
+++ domain/src/main/java/com/example/domain/repository/FriendListRepository.kt
@@ -1,4 +1,10 @@
 package com.example.domain.repository
 
+import com.example.domain.entity.ResponseFriendListEntity
+
 class FriendListRepository {
+    fun getFriendList (friendListId: Int) : @NonNull Single<Response<ResponseFriendListEntity>> =
+        ApiService.getPostDetail(ACCESS_TOKEN, friendListId)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
