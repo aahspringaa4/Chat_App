b9623c9e003dab2fda86d99924ef4a64fe9913ee
유현명 <aahspringaa4@naver.com>
2022-01-10 16:15:56 +0900

    📑 :: Chat Repository 구현

--- domain/src/main/java/com/example/domain/repository/ChatRepository.kt
+++ domain/src/main/java/com/example/domain/repository/ChatRepository.kt
@@ -1,4 +1,10 @@
 package com.example.domain.repository
 
+import com.example.domain.entity.ResponseChattingListEntity
+
 class ChatRepository {
+    fun ChattingSocket (noticeId: Int) : @NonNull Single<Response<ResponseChattingEntity>> =
+        ApiService.getPostDetail(ACCESS_TOKEN, noticeId)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
