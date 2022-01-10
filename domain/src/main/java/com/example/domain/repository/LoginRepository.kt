f8c3483e850068f4ce63b4c30681d880c2c5062d
유현명 <aahspringaa4@naver.com>
2022-01-10 16:16:38 +0900

    📑 :: Login Repository 구현

--- domain/src/main/java/com/example/domain/repository/LoginRepository.kt
+++ domain/src/main/java/com/example/domain/repository/LoginRepository.kt
@@ -1,4 +1,10 @@
 package com.example.domain.repository
 
+import javax.security.auth.login.LoginException
+
 class LoginRepository {
+    fun PostLogin () : @NonNull Single<Response<LoginException>> =
+        ApiService.getPostDetail(ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
