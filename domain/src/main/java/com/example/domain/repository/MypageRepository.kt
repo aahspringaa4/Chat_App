228aa0cf8eb93ba852f515519eb0e1e9225d1238
유현명 <aahspringaa4@naver.com>
2022-01-10 16:16:50 +0900

    📑 :: Mypage Repository 구현

--- domain/src/main/java/com/example/domain/repository/MypageRepository.kt
+++ domain/src/main/java/com/example/domain/repository/MypageRepository.kt
@@ -1,4 +1,8 @@
 package com.example.domain.repository
 
 class MypageRepository {
+    fun getMypage(mypageId: Int) : @NonNull Single<Response<ResponseMypageEntity>> =
+        ApiService.getPostDetail(ACCESS_TOKEN, mypageId)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
