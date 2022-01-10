ee6a997eec742c708defbaf8666416b29a4efca9
유현명 <aahspringaa4@naver.com>
2022-01-10 16:16:07 +0900

    📑 :: 채팅 리스트 Repository 구현

--- domain/src/main/java/com/example/domain/repository/ChattingListRepository.kt
+++ domain/src/main/java/com/example/domain/repository/ChattingListRepository.kt
@@ -1,4 +1,10 @@
 package com.example.domain.repository
 
+import com.example.domain.entity.ResponseChattingListEntity
+
 class ChattingListRepository {
+    fun getChattingList (chattingRoomId: Int) : @NonNull Single<Response<ResponseChattingListEntity>> =
+        ApiService.getPostDetail(ACCESS_TOKEN, chattingRoomId)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
