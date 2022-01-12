691b2972a0fb6198fa34e12d38551606d37c28d5
유현명 <aahspringaa4@naver.com>
2022-01-12 09:04:12 +0900

    📑:: DI Application 구현

--- presentation/src/main/java/di/ChatApplication.kt
+++ presentation/src/main/java/di/ChatApplication.kt
@@ -1,2 +1,27 @@
 package di
 
+import android.app.Application
+import di.module.ChatMoudle
+import di.module.ChattingListMoudle
+import di.module.LoginMoudle
+import di.module.MypageModule
+
+class ChatApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@ChatApplication)
+
+            modules(
+                listOf(
+                    ChatMoudle,
+                    ChattingListMoudle,
+                    FriendListMoudle,
+                    LoginMoudle,
+                    MypageModule
+                )
+            )
+        }
+    }
+}
